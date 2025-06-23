8ad3ce8844d941cd64d5cd5fd33adfb8e099700e
Aditya Singh <adityasinghofficial06@gmail.com>
2025-06-23 23:22:52 +0530

    Intial Edits to ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Simpli
+![Sempli Logo](https://raw.githubusercontent.com/your-username/sempli/main/logo.png)
+
+# Sempli
+
+**A smart, minimal Chrome extension that instantly simplifies the meaning of any word you highlight.**
+
+---
---
+++ logic.js
@@ -0,0 +1,25 @@
+// will be writing the logic here of the js //
+console.log("hello world");
+// document.querySelector(".submit").addEventListener("click", () => {
+//   alert("word sent");
+//   const word = document.querySelector(".word").value;
+//   console.log(word);
+
+//   setTimeout(() => {
+//     document.querySelector(".word").innerHTML = " change kardiya";
+//     console.log("I am here");
+//   }, "1000");
+// });
+
+const btn = document.querySelector(".send");
+
+btn.addEventListener("click", () => {
+  if (window.getSelection) {
+    var text = "";
+    text = window.getSelection().toString().toLowerCase();
+    console.log(text);
+    console.log("here");
+  } else {
+    alert("Select something first");
+  }
+});
